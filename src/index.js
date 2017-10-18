ad59b385de52823fdfef605e4c3fc81132af2629
Ben Mansley <ben@eduroam-222-219.uea.ac.uk>
2017-10-18 19:20:18 +0100

    Final version (ish)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 class App {
 
   constructor() {
+    this.menu = document.querySelector('.menu');
+    this.menuToggle = document.querySelector('.menu-toggle');
+    this.toggleMenu = this.toggleMenu.bind(this);
+    
+    let self = this;
+    this.menuToggle.addEventListener('click', self.toggleMenu);
+    
+  }
 
+  toggleMenu(event) {
+    console.log();
+    this.menu.classList.toggle('hidden');
   }
 }
 
